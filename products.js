2af1ce58d06439daabbcb808ca45444f601af7e3
Uzma Shaikh <uzma1109@yahoo.com>
2022-02-18 15:42:57 +0400

    yay

---
+++ products.js
@@ -0,0 +1,19 @@
+let products = [
+    {
+      "id": 1001,
+      "title": "Cat Food, 25lb bag",
+      "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+      "price": 20.00,
+      "image": "images/cat-food.jpg",
+      "availableInventory": 10,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "images/yarn.png",
+      "availableInventory": 7,
+      "rating": 3
+    }]
